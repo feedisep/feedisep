a5c69cf7f5a8d606425f5f4eff3142119e665195
Antoine <antoafan@gmail.com>
2019-05-20 10:44:22 +0200

    [ADD] main-content : research bar [REF] ticket-full : title bar [REF] ticket-min : etat -> state refactor

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import {MatIconRegistry} from '@angular/material';
 import {DomSanitizer} from '@angular/platform-browser';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import {FormControl} from '@angular/forms';
 import {Ticket} from '../Ticket';
+
+export interface User {
+  name: string;
+}
 
 @Component({
   selector: 'app-search-zone',
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.css']
 })
+
+
 export class MainContentComponent implements OnInit {
 
   // @ts-ignore
@@ -26,4 +35,33 @@ export class MainContentComponent implements OnInit {
   ngOnInit() {
   }
 
+}
+
+export class AutocompleteDisplayExample implements OnInit {
+  myControl = new FormControl();
+  options: User[] = [
+    {name: 'Mary'},
+    {name: 'Shelley'},
+    {name: 'Igor'}
+  ];
+  filteredOptions: Observable<User[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith<string | User>(''),
+        map(value => typeof value === 'string' ? value : value.name),
+        map(name => name ? this._filter(name) : this.options.slice())
+      );
+  }
+
+  displayFn(user?: User): string | undefined {
+    return user ? user.name : undefined;
+  }
+
+  private _filter(name: string): User[] {
+    const filterValue = name.toLowerCase();
+
+    return this.options.filter(option => option.name.toLowerCase().indexOf(filterValue) === 0);
+  }
 }
